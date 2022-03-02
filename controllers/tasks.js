e6f21281c6c169ce1016a77064d590d6e83c1ec9
Błażej Niewiadomski <blaz.nie@protonmail.com>
2022-03-03 00:07:22 +0100

    somewhat usable API done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,25 +2,64 @@ const express = require('express')
 const Task = require('../models/task')
 
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks }) // { tasks } == { tasks: tasks }
+  } catch (error) {
+    res.status(500).json({ msg: error.stack })
+  }
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({ task })
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error.stack })
+  }
 }
 
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id:taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id:taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error.stack })
+  }
 }
 
-const updateTask = (req, res) => {
-  res.send('upate task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true
+    })
+    res.status(200).json({ id: taskID, data: req.body })
+  } catch (error) {
+    res.status(500).json({ msg: error.stack })
+  }
 }
 
-const getTask = (req, res) => {
-  res.send('get task')
+const getTask = async (req, res) => {
+  try {
+    const { id:taskID } = req.params
+    const task = await Task.findOne({ _id:taskID })
+    res.json({ id: req.params.id })
+    if (!task) {
+      // in case request id isnt in the databae
+      return res.status(400).json({ msg: `No task with id : ${taskID}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    // in case request doesnt match the schema
+    res.status(500).json({ msg: error.stack })
+  }
 }
 
 
--- models/task.js
+++ models/task.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose')
 
+
+// schema defines what fields are expected in the database
+// if any other fields are passed, they are ignored
+// mongodb documents dont have predefined structure
+// schemas fix that
 const TaskSchema = new mongoose.Schema({
-  name:String,
-  completed:Boolean
+  name: {
+    type:String,
+    // [<ifrequired>, <error message>/>]
+    required:[true, 'must provide a name'],
+    trim:true,
+    maxlength:[20, 'name cannot be more than 20 characters']
+  },
+  completed: {
+    type:Boolean,
+    default:false
+  }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
